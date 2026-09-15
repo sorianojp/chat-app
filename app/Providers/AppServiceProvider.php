2d1831000b54f3f5852723afa4e89657bb7eada7
sorianojp <johnpaul.soriano.6197@gmail.com>
2026-09-15 11:57:10 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace App\Providers;
 
-use App\Events\MessageCreated;
-use App\Listeners\QueueMessagePushNotifications;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -26,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(MessageCreated::class, QueueMessagePushNotifications::class);
         $this->configureDefaults();
     }
 
